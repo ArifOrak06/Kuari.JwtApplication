3fefc805e7160bece05a240736e5a8040185af61
Arif ORAK <ariforak91@gmail.com>
2023-01-19 22:31:37 +0300

    Core Katmanı oluşturuldu, entity class'lar en temel düzeyde oluşturuldu.Back Projesi içerisinde Onion Mimariye göre klasör yapısı oluşturulacak şekilde Back Projesi oluşturuldu.

---
+++ Kuari.JwtApplication.Back/Core/Domain/Category.cs
@@ -0,0 +1,13 @@
+﻿namespace Kuari.JwtApplication.Back.Core.Domain
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string? Definition { get; set; }
+        public List<Product> Products { get; set; }
+        public Category()
+        {
+            Products = new List<Product>();
+        }
+    }
+}
