4e09f955ddcab75e62198e6cd9d582bcd9b1c857
Arif ORAK <ariforak91@gmail.com>
2023-01-19 23:59:43 +0300

    GenericRepository interface'i en temel haliyle oluşturuldu, Core Katmanı inşa edildi.Sözleşme Core/Application, Manager class Persistance Katmanında inşa edilmiştir.

---
+++ Kuari.JwtApplication.Back/Core/Application/İnterfaces/IRepository{T}.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq.Expressions;
+
+namespace Kuari.JwtApplication.Back.Core.Application.İnterfaces
+{
+    public interface IRepository<T> where T : class, new()
+    {
+        Task<List<T>> GetAllAsync();
+        Task<T?> GetByIdAsync(object id); // Update işlemi için Tracking açık
+        Task<T?> GetByFilterAsync(Expression<Func<T, bool>> filter); // asNoTracking ürün detayının listelenmesi durumlarında kullanılabilir.
+        Task CreateAsync(T entity);
+        Task UpdateAsync(T entity);
+        Task DeleteAsync(T entity);
+    }
+}
---
+++ Kuari.JwtApplication.Back/Persistance/Repositories/Repository{T}.cs
@@ -0,0 +1,50 @@
+﻿using Kuari.JwtApplication.Back.Core.Application.İnterfaces;
+using Kuari.JwtApplication.Back.Persistance.Contexts;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace Kuari.JwtApplication.Back.Persistance.Repositories
+{
+    public class Repository<T> : IRepository<T> where T : class, new()
+    {
+        private readonly JwtApplicationContext _context;
+
+        public Repository(JwtApplicationContext context)
+        {
+            _context = context;
+        }
+
+        public async Task CreateAsync(T entity)
+        {
+            await this._context.Set<T>().AddAsync(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(T entity)
+        {
+            _context.Set<T>().Remove(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<List<T>> GetAllAsync()
+        {
+            return await _context.Set<T>().AsNoTracking().ToListAsync();
+        }
+
+        public async Task<T?> GetByFilterAsync(Expression<Func<T, bool>> filter)
+        {
+            return await _context.Set<T>().AsNoTracking().Where(filter).SingleOrDefaultAsync();
+        }
+
+        public async Task<T?> GetByIdAsync(object id)
+        {
+            return await _context.Set<T>().FindAsync(id);
+        }
+
+        public async Task UpdateAsync(T entity)
+        {
+            _context.Set<T>().Update(entity);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
